139b9a62a9bb8fbee6e54b6a61c9e5ee7309cd23
r-1c4rd0 <ricardosantiagocwb@gmail.com>
2020-11-18 01:20:23 -0300

    alteraando a classe home e incluido as imagens

---
+++ epastoral/lib/components/Dashboard.dart
@@ -0,0 +1,42 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
+
+class Dashboard extends StatelessWidget {
+  final  String folderPlay,  i;
+
+
+
+  const Dashboard({Key key, this.folderPlay, this.i}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      CarouselSlider(
+        options: CarouselOptions(
+          height: 400.0,
+          aspectRatio: 16 / 9,
+          viewportFraction: 0.8,
+          initialPage: 0,
+          enlargeCenterPage: true,
+          scrollDirection: Axis.horizontal,
+          enableInfiniteScroll: true,
+          reverse: false,
+          autoPlay: true,
+          autoPlayInterval: Duration(seconds: 3),
+          autoPlayAnimationDuration: Duration(milliseconds: 500),
+          autoPlayCurve: Curves.fastOutSlowIn,
+        ),
+        items: [
+          Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18.0),
+                image: DecorationImage(
+                  image: AssetImage('images/$folderPlay/$i.png'),
+                ),
+              )),
+        ],
+      );
+  }
+
+}
---
+++ epastoral/lib/models/basket.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Basket extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
